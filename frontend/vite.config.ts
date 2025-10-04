a21d9e5a0b187975fd1a2e084ee7f527a6ef6f46
mkarachalios-1 <127642270+mkarachalios-1@users.noreply.github.com>
2025-10-04 21:35:36 +0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Use a fallback for GitHub Actions where import.meta.env may not be defined
+const base = process.env.VITE_BASE || '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE || '/'
+  base
 })
